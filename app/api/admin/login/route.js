ca9cc5628f9b9b2437d4cbadab90523f0f98e567
nurmohammadfadil <nurmohammadfadil@gmail.com>
2025-09-18 20:38:46 +0700

    Push Hari Ke 2

--- app/api/admin/login/route.js
+++ app/api/admin/login/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { Admin, sequelize } from "../../../../lib/sequelize";
 
 const { Op } = require("sequelize");
